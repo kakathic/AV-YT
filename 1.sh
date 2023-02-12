16f6c6146d75ac9844af1b3d1dfebf4f176a7144
kakathic <kakathic@gmail.com>
2023-02-12 08:32:04 +0700

    Uploaded

--- 1.sh
+++ 1.sh
@@ -6,8 +6,9 @@ rm -fr /data/user/0/com.termux/files/usr/etc/apt/sources.list.d/*
 echo username=kakathic
 echo password=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
 git config --global user.email "kakathic@gmail.com"
-git config --global user.name "kakathic"
-git remote add https://kakathic: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@github.com/kakathic/AV-YT.git
+git config --global user.name "kakathic" 
+git remote set-url origin git@github.com:kakathic/AV-YT.git
+You can
 #git config --global github.user kakathic
 #git config --global github.token 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
 #termux-setup-storage
@@ -20,4 +21,4 @@ git add .
 git commit -m "Uploaded"
 git remote add origin https://github.com/kakathic/AV-YT.git
 git pull origin Vip
-git push origin Vip < kakathic
+git push origin Vip
