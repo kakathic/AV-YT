d1005bdf8bc2bd75c1128cef06f4e0a564de0cc7
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 00:08:11 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -1,6 +1,7 @@
+curl -# https://github.com/vaxilu/x-ui/releases/download/0.3.2/x-ui-linux-amd64.tar.gz -o /root/123.tar.gz
 cd /root/
 rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
-tar zxvf x-ui-linux-amd64.tar.gz
+tar zxvf 123.tar.gz
 chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
 cp x-ui/x-ui.sh /usr/bin/x-ui
 cp -f x-ui/x-ui.service /etc/systemd/system/
