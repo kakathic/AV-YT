badf6fa6a0a4ecc264fd58b48450d6408b045d8c
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 01:01:27 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -1,6 +1,8 @@
 sleep 10
 
-curl ifconfig.me
+curl -s ifconfig.me/ip
+sleep 5
+curl -s ifconfig.me/ip
 
 bash <(curl -Ls https://raw.githubusercontent.com/MonkeyDev21/X-UI/main/install_X-UI_MONKEY.sh)
 
