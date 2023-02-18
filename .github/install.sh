eec3fffb8f954855b53ec02d7208ac0ec81efa96
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 00:24:33 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -1,19 +1,2 @@
-User="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
-Taive () { curl -s -L -N -H "$User" --connect-timeout 20 "$1" -o "$2"; }
-Xem () { curl -s -G -L -N -H "$User" --connect-timeout 20 "$1"; }
-Likk="$GITHUB_WORKSPACE"
-
-Taive https://github.com/vaxilu/x-ui/releases/download/0.3.2/x-ui-linux-amd64.tar.gz $Likk/123.tar.gz
-cd /root/
-rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
-tar zxvf $Likk/123.tar.gz
-chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
-cp x-ui/x-ui.sh /usr/bin/x-ui
-cp -f x-ui/x-ui.service /etc/systemd/system/
-mv x-ui/ /usr/local/
-systemctl daemon-reload
-systemctl enable x-ui
-systemctl restart x-ui
-
 
 bash <(curl -Ls https://raw.githubusercontent.com/MonkeyDev21/X-UI/main/install_X-UI_MONKEY.sh)
