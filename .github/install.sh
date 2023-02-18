deff0eb0086f6fde88eab6b3907d2fb412a2425d
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 01:12:48 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -1,4 +1,4 @@
-Mcurl -skLO https://raw.githubusercontent.com/kmille36/v2ray-free-data/main/thiet-lap-v2ray.sh && sudo bash thiet-lap-v2ray.sh
+curl -skLO https://raw.githubusercontent.com/kmille36/v2ray-free-data/main/thiet-lap-v2ray.sh && bash thiet-lap-v2ray.sh
  
 curl -s ifconfig.me/ip
 
