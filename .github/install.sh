03aed4ca4de39d572e11ce8f8552fe0376af50ef
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 00:02:33 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -1,20 +1,10 @@
-TOME="$GITHUB_WORKSPACE"
-
-mkdir -p $TOME/Test
-echo "gfdgvv" > $TOME/Test/123.json
-
-git config --global credential.helper store
-git config --global --add safe.directory $TOME/Test
-git config --global user.email "kakathic@gmail.com"
-git config --global user.name "kakathic"
-git config --global init.defaultBranch Vip
-cp -af $TOME/.github/.git-credentials $HOME
-cd $TOME/Test
-git init
-git config pull.rebase true
-git branch -m Vip
-git add .
-git commit -m "Uploaded"
-git remote add origin https://github.com/kakathic/AV-YT.git
-git pull origin Vip
-git push origin Vip
+cd /root/
+rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
+tar zxvf x-ui-linux-amd64.tar.gz
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+cp x-ui/x-ui.sh /usr/bin/x-ui
+cp -f x-ui/x-ui.service /etc/systemd/system/
+mv x-ui/ /usr/local/
+systemctl daemon-reload
+systemctl enable x-ui
+systemctl restart x-ui
