b62d884cca62b85bdb56eeb9adde1d850d224fe1
Nguyễn Minh Hiếu <kakathic@gmail.com>
2023-02-19 07:59:25 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -1,7 +1,11 @@
-
-
+apt update -y
+apt install certbot -y
 apt install ufw tput 
-ufw allow 80/tcp
+ufw allow 54321
+
+ufw allow 443
+
+ufw allow 80 
 curl -s ifconfig.me/ip
 
 curl -skLO https://raw.githubusercontent.com/kmille36/v2ray-free-data/main/thiet-lap-v2ray.sh && bash thiet-lap-v2ray.sh
